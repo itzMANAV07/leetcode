7c67b60986c261adfc47fa1774fdcd48e6c882b9
Manav <myself.manav07@gmail.com>
2026-08-09 23:23:34 +0530

    Fix isValid function to detect invalid parentheses

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -3,5 +3,6 @@ class Solution:
         while len(s) > 0:
             l = len(s)
             s = s.replace('()','').replace('{}','').replace('[]','')
+            
             if l==len(s): return False
         return True
